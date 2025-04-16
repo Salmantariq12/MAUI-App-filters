10a668b9e457ffc22bffce662ea0a5b703efdf32
Salman Tariq <salmantariqshaykh001@gmail.com>
2025-04-16 18:27:18 +0500

    Add files via upload

---
+++ MAUI APP Filters/Models/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppFilter.Models
+{
+    public class Settings
+    {
+        public List<string> AllowedDomains { get; set; }
+        public List<TimeSlot> TimeSlots { get; set; }
+    }
+
+    public class TimeSlot
+    {
+        public DayOfWeek Day { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
